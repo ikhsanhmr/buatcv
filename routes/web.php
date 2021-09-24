2f6b219db16d1ef5a7cb84209a0038ddbe77574b
gagassss <gagassss@protonmail.com>
2021-09-25 04:41:51 +0700

    basic auth without email verif

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/contact', function () { return view('frontend.pages.contact'); });
 // });
 
 Route::get('/index', [IndexController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
