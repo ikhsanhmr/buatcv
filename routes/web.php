323d6bfb941d09cb3ecca7be41a846a2cde56e7f
gagassss <gagassss@protonmail.com>
2021-09-25 05:43:14 +0700

    add email verification and prevent unverified user to access private/profile page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,6 +27,6 @@ Route::get('/contact', function () { return view('frontend.pages.contact'); });
 
 Route::get('/index', [IndexController::class, 'index']);
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
